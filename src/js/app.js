8b8fb7c1d772b47085e1076e4beb8d850ee71071
heavenlybilly <heavenlybilly@gmail.com>
2023-05-28 13:57:21 +0300

    feat: корзина и localStorage

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,8 @@
+$(document).ready(function () {
+    updateCart()
+})
+
+
 // меню
 $('.nav-menu-link').click(function (e) {
     e.stopPropagation()
@@ -25,3 +30,44 @@ $('.tabs-panel-item').click(function (e) {
     $('.tabs-panel-item').attr('data-is-active', '0')
     $(`.tabs-panel-item[data-target="${target}"]`).attr('data-is-active', '1')
 })
+
+// корзина
+$('.card').find('.card-cart').click(function (e) {
+    const $root = $(this).closest('.card')
+    const id = $root.attr('data-id')
+
+    let count = localStorage.getItem(id)
+
+    if (count === null) {
+        count = 1
+    }
+    else {
+        count++
+    }
+
+    localStorage.setItem(id, count)
+    updateCart()
+})
+
+function updateCart() {
+    let count = 0
+
+    for(let key in localStorage) {
+        if (!localStorage.hasOwnProperty(key)) {
+            continue; // пропустит такие ключи, как "setItem", "getItem" и так далее
+        }
+
+        count += Number(localStorage.getItem(key))
+    }
+
+    const $cartCount = $('#cart').find('.nav-cart-count')
+
+    if (count > 0) {
+        $cartCount.attr('data-is-active', '1')
+        $cartCount.text(count)
+    }
+    else {
+        $cartCount.attr('data-is-active', '0')
+        $cartCount.text('')
+    }
+}
