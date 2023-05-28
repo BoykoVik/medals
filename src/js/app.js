c11f292f97a3a0e4becf6905524fecf486550c9a
heavenlybilly <heavenlybilly@gmail.com>
2023-05-28 11:50:50 +0300

    feat: вкладки

--- src/js/app.js
+++ src/js/app.js
@@ -12,3 +12,16 @@ $('.nav-menu-link').click(function (e) {
         $container.attr('data-is-opened', '1')
     }
 })
+
+// вкладки
+$('.tabs-panel-item').click(function (e) {
+    e.preventDefault()
+
+    const target = $(this).attr('data-target')
+
+    $('.tabs-content-item').attr('data-is-active', '0')
+    $(`.tabs-content-item[data-target="${target}"]`).attr('data-is-active', '1')
+
+    $('.tabs-panel-item').attr('data-is-active', '0')
+    $(`.tabs-panel-item[data-target="${target}"]`).attr('data-is-active', '1')
+})
