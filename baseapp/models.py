1ad57e98cbb693e612489dc130f5e862936eb455
BoykoVik <gexar47@gmail.com>
2023-06-11 16:35:36 +0300

    back

--- baseapp/models.py
+++ baseapp/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
 # Create your models here.
+
+# Модель категорий
+class Categories(models.Model):
+    title = models.CharField(blank=False, max_length=80, verbose_name='Наименование категории')
+    image = models.ImageField(blank=True, upload_to='categories/', verbose_name='Главное изображение категории')
+    
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['title']
+
+# Модель товаров
+class Product(models.Model):
+    category = models.ForeignKey(Categories, on_delete=models.CASCADE, verbose_name='Категория', related_name="category")
+    title = models.CharField(blank=False, max_length=80, verbose_name='Наименование товара')
+    image = models.ImageField(blank=True, upload_to='products_imgs/', verbose_name='Главное изображение товара')
+    
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+        
+    def __str__(self):
+        return str(self.title)
+
+# Модель дополнительных изображений товаров
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name='товар', related_name="product")
+    image = models.ImageField(blank=True, upload_to='product_imgs/', verbose_name='Дополнительное изображение')
+    
+    class Meta:
+        verbose_name = 'Дополнительное изображение'
+        verbose_name_plural = 'Дополнительные изображения'
+    def __str__(self):
+        return str(self.image)
