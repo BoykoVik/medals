b005882caef96da1327ef1d08015c6f726dffb5f
BoykoVik <gexar47@gmail.com>
2023-07-02 18:40:36 +0300

    back

--- ordersapp/views.py
+++ ordersapp/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from baseapp.views import categorytonav
 # Create your views here.
 def cart(request):
+    broads = []
+    broads.append(f'<li class="breadcrumb-item active">Корзина</li>')
     return render(request, 'ordersapp/cart.html', {
         'categories': categorytonav(),
+        'broads': broads,
     })
