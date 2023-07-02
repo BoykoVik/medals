f558e0f7d9f570f2cad6cb756c483a3ce2b11c62
BoykoVik <gexar47@gmail.com>
2023-07-02 19:00:18 +0300

    back

--- apiapp/urls.py
+++ apiapp/urls.py
@@ -4,9 +4,6 @@ from . import views
 app_name = 'apiapp'
 
 urlpatterns = [
-    path('products-new', views.products_new, name='products-new'),
-    path('products-hit', views.products_hit, name='products-hit'),
-    path('products-sale', views.products_sale, name='products-sale'),
     path('products-list', views.products_list, name='products-list'),
     path('products-list-selected', views.products_list_selected, name='products-list-selected'),
     path('cart/items', views.cartitems, name='cartitems'),
--- apiapp/views.py
+++ apiapp/views.py
@@ -5,24 +5,12 @@ from django.core import serializers
 from baseapp.models import Categories, Product, ProductImage, Parameters
 # Create your views here.
 
-def products_new(request):
-    objects = Product.objects.filter(is_new = True)
-    return JsonResponse(serialise_data(objects), safe=False, encoder=DjangoJSONEncoder)
-    
-def products_hit(request):
-    objects = Product.objects.filter(is_hit = True)
-    return JsonResponse(serialise_data(objects), safe=False, encoder=DjangoJSONEncoder)
-
-def products_sale(request):
-    objects = Product.objects.filter(is_sale = True)
-    return JsonResponse(serialise_data(objects), safe=False, encoder=DjangoJSONEncoder)
-
 def products_list(request):
     if 'category' in request.GET:
         categoryId = request.GET['category']
     else:
         categoryId = '1'
-    objects = Product.objects.filter(category = categoryId)[:4]
+    objects = Product.objects.filter(category = categoryId)[:8]
     return JsonResponse(serialise_data(objects), safe=False, encoder=DjangoJSONEncoder)
 
 def products_list_selected(request):
@@ -104,9 +92,7 @@ def getparameterlabel(request):#название параметра и одно 
         }
     return JsonResponse(answer, safe=False, encoder=DjangoJSONEncoder)
 
-def productdetail(request, id):
-    objects_serialized_data = []
-    
+def productdetail(request, id):    
     product = get_object_or_404(Product, pk = id)
     if product.needparameters:
         answer =  {
